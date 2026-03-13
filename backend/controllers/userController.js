bb813f670bdb58a21c089b0eff0fb9472d753b6a
Devanshi12srivastava <devanshisrivastava164@gmail.com>
2026-03-13 17:55:56 +0530

    appointment apii done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,9 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
-import {v2 as cloudinary} from 'cloudinary'
+import { v2 as cloudinary } from "cloudinary";
+import doctorModel from "../models/doctorModel.js";
+import appointmentModel from "../models/appointementModel.js";
 
 const registerUser = async (req, res) => {
   try {
@@ -82,32 +84,80 @@ const getProfileData = async (req, res) => {
 };
 
 //api for update
-const userProfielUpdate = async(req, res) => {
-
+const userProfileUpdate = async (req, res) => {
   try {
-  const userId = req.userId
+    const userId = req.userId;
     const { name, phone, address, dob } = req.body;
     const user = await userModel.findById(userId);
-console.log("user",user);
-    const imageFile = req.file
-        console.log("body",req.body);
-    if(!name || !phone || !dob || !address){
-        return res.json({success:false,messag:"Data missing"})
-
+    console.log("user", user);
+    const imageFile = req.file;
+    console.log("body", req.body);
+    if (!name || !phone || !dob || !address) {
+      return res.json({ success: false, messag: "Data missing" });
     }
-    const updatedUser=await userModel.findByIdAndUpdate(userId,{name,phone,address,dob},  { new: true })
-    console.log("updates",updatedUser);
-    if(imageFile){
-        const imageUpload =await cloudinary.uploader.upload(imageFile.path,{resource_type:'image'})
-        const imageURL= imageUpload.secure_url
+    const updatedUser = await userModel.findByIdAndUpdate(
+      userId,
+      { name, phone, address, dob },
+      { new: true },
+    );
+    console.log("updates", updatedUser);
+    if (imageFile) {
+      const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+        resource_type: "image",
+      });
+      const imageURL = imageUpload.secure_url;
 
-        await userModel.findByIdAndUpdate(userId,{image:imageURL})
+      await userModel.findByIdAndUpdate(userId, { image: imageURL });
     }
-res.json({success:true,message:"profile updated"})
+    res.json({ success: true, message: "profile updated" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+//book appointemnet api
+
+const bookAppointment = async (req, res) => {
+  try {
+    const { userId, docId, slotDate, slotTime } = req.body;
+
+    const docData = await doctorModel.findById(docId).select("-password");
+    if (!docData.available) {
+      return res.json({ success: false, message: "doctor not available" });
+    }
+    let slots_booked = docData.slots_booked;
+    //slots availablity
+    if (slots_booked[slotDate]) {
+      if (slots_booked[slotDate].includes(slotTime)) {
+        return res.json({ success: false, message: "slot not available" });
+      } else {
+        slots_booked[slotDate].push(slotTime);
+      }
+    } else {
+      slots_booked[slotDate] = [];
+      slots_booked[slotDate].push(slotTime);
+    }
+    const userData =await userModel.findById(userId).select('-password')
+    delete docData.slots_booked
+
+    const appointmentData={
+      userId,docId,userData,docData,amount:docData.fees,
+      slotTime,slotDate,date:Date.now()
+    }
+
+const newAppointment = new appointmentModel(appointmentData)
+await newAppointment.save()
+
+
+//save new slot data in docData
+
+await doctorModel.findByIdAndUpdate(docId,{slots_booked})
+res.json({success:true,message:"appointmnet booked"})
 
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: error.message });
   }
 };
-export { registerUser, loginUser, getProfileData,userProfielUpdate  };
+
+export { registerUser, loginUser, getProfileData, userProfileUpdate };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getProfileData, loginUser, registerUser, userProfielUpdate } from "../controllers/userController.js"
+import { getProfileData, loginUser, registerUser, userProfileUpdate } from "../controllers/userController.js"
 import authuser from "../middleware/authuser.js"
 import upload from "../middleware/multer.js"
 
@@ -9,5 +9,5 @@ const userRouter=express.Router()
 userRouter.post('/register-user',registerUser)
 userRouter.post('/login-user',loginUser)
 userRouter.get('/get-profile',authuser,getProfileData)
-userRouter.patch('/update-profile',upload.single('image'),authuser,userProfielUpdate)
+userRouter.post('/update-profile',upload.single('image'),authuser,userProfileUpdate)
 export default userRouter
